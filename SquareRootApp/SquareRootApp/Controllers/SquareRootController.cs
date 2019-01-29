21119d9e18a1124997d99ab567c43c226b929fa0
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-29 14:23:53 +0100

    Initial commit; får fixa så resultatet visas bl.a.

---
+++ SquareRootApp/SquareRootApp/Controllers/SquareRootController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRootApp.Controllers
+{
+    public class SquareRootController : Controller
+    {
+        [Route("SquareRoot")]
+        public IActionResult SquareRoot(int? number)
+        {
+            return Ok(Math.Sqrt((int)number));
+        }
+    }
+}
---
+++ SquareRootApp/SquareRootApp/js/square-root.js
@@ -0,0 +1,21 @@
+﻿
+async function SquareRoot() {
+    //let result = document.getElementById("result");
+
+    let number = document.getElementById("number").value;
+
+    let response = await fetch(`api/squareroot?number=${number}`);
+
+    if (response.status == 200) {
+        result = await response.json();
+        errorMessage = "";
+    }
+    else if (response.status == 400) {
+        result = "";
+        errorMessage = await response.text();
+    } else {
+        result = "";
+        errorMessage = `Unexpected (status code = ${response.status})`;
+    }
+
+}
